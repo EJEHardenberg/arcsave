4a44295eff08b4a75d33044b007e43947792d8a0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-08 19:03:30 -0500

    Peeep support

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@
 var arcGenerator = {
   genArc: function(url) {
     /* If on twitter also send to tweetsave */
-    if(url.match(/twitter.com\/[^/]+status/)){
+    if(url.match(/twitter.com\/[^\/]+\/status/)){
       setTimeout(function(){
         open('http://tweetsave.com/?tweet='+encodeURIComponent(url))
       },10)
@@ -14,6 +14,27 @@ var arcGenerator = {
     } 
     setTimeout(function(){
       open('https://archive.today/?run=1&url='+encodeURIComponent(url))  
+    },10)    
+    //Because 1 is never enough, peep needs a post request, so FORM it is
+    setTimeout(function(){
+      var form = document.createElement('form')
+      form.method = 'POST'
+      form.action = 'http://www.peeep.us/upload.php'
+      form.setAttribute('enctype', "application/x-www-form-urlencoded")
+
+      var urlInput = document.createElement('input')
+      urlInput.setAttribute('name','r_url')
+      
+      urlInput.setAttribute('value',url)
+      urlInput.textContent = url
+
+      form.appendChild(urlInput)
+
+      var formurl = 'data:text/html;charset=utf8,';
+      formurl += encodeURIComponent(form.outerHTML);
+      formurl += encodeURIComponent('<h1><marquee direction=right>Archiving.Please Wait</marquee></h1><script>document.forms[0].submit();</script>');
+
+      open(formurl)
     },10)    
   },
 };
